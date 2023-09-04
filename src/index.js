b5134174977a0f8a927923d2c1a5340bad9094df
Saikilito <saikilito@pop-os.localdomain>
2023-09-04 19:40:03 -0400

    createUsers

--- src/index.js
+++ src/index.js
@@ -10,3 +10,19 @@
 // }
 
 let db = [];
+
+const createUser = ({
+  id,
+  name,
+  lastname,
+  dni,
+  year,
+  createdAt,
+  deletedAt,
+  updateAt,
+}) => {
+  const newUser =
+    (id, name, lastname, dni, year, createdAt, deletedAt, updateAt);
+  db.push(newUser);
+  return newUser;
+};
